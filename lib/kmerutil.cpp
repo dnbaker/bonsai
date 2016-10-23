52444125f09303255db83e63467bb5c21b49caf1
Daniel Baker <dnb@cs.jhu.edu>
2016-10-23 01:00:36 -0400

    Add forgotten include.

---
+++ lib/kmerutil.cpp
@@ -0,0 +1,7 @@
+#include "kmerutil.h"
+
+namespace kpg {
+//Set a return kmer (overlap_kmer) for the matched sequence, and len_pos, which contains the length of the kmer
+//and the index of the kmer match in ka as the first 16 and last 16 bits of a uint32_t, respectively.
+
+} // namespace kpg
