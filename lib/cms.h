2dfe6d4668d86b9318f9668762253304f259f6b5
Daniel Baker <dnb@cs.jhu.edu>
2016-10-25 08:45:42 -0400

    Correct 2D array access

--- lib/cms.h
+++ lib/cms.h
@@ -28,15 +28,15 @@ static inline size_t roundup64(size_t x) {
 template<typename T, size_t ns=4>
 struct cms_t {
     const size_t sz_;
-    std::vector<T> bits_; // 2-d array of bits.
-    uint32_t seeds_[ns];
+    std::vector<T> bits_; // 2-d array of bits. Access position j within array i via bits_[j + i *sz_]
+    uint64_t seeds_[ns];
     const size_t mask_;
 
     cms_t(size_t sz, uint32_t seedseed=1337): // Lucky number
       sz_(roundup64(sz)), bits_(ns * sz_, 0), mask_(sz_ - 1)
     {
         srand(seedseed);
-        for(auto &seed: seeds_) seed = rand();
+        for(auto &seed: seeds_) seed = ((uint64_t)rand() << 32) | rand();
     }
     INLINE void add(uint64_t hashval, int val) {
         // Multiply a produced hash
@@ -50,7 +50,7 @@ struct cms_t {
         unsigned i(0);
         for(auto seed: seeds_) {
             const T val((hashval ^ seed) & mask_);
-            if(bits_[val + i * ns] < ret) ret = bits_[val + i * ns];
+            if(bits_[val + i * sz_] < ret) ret = bits_[val + i * sz_];
             ++i;
         }
         return ret;
