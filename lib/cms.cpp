e9122b4976a82ee4c433a99b9f41493c82cdac48
Daniel Baker <dnb@cs.jhu.edu>
2016-10-25 19:43:30 -0400

    Add forgotten cpp file for testing.

---
+++ lib/cms.cpp
@@ -0,0 +1,9 @@
+#include "cms.h"
+
+using namespace kpg;
+
+int main(void) {
+    cms_t<uint8_t> test(65536);
+    for(size_t i(0);i<100000; ++i) test.add(i);
+    fprintf(stderr, "Estimate %u.\n", (unsigned)test.query(1337));
+}
