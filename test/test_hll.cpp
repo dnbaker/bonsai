69fe9098af9a84c654e29eed0a1bdbe128ef58e0
dnbh <dnb@cs.jhu.edu>
2017-02-01 09:43:25 -0500

    Slightly clean up test suite after seeing accuracy with np=24, n>=1e12

--- test/test_hll.cpp
+++ test/test_hll.cpp
@@ -17,13 +17,6 @@ int within_bounds(hll::hll_t &hll, double val) {
     return std::abs(hll.report() - val) <= hll.est_err();
 }
 
-std::vector<std::pair<uint64_t, uint64_t>> pairs {
-    {17, 24},
-    {8, 14},
-    {12, 17},
-    {15, 19},
-    {22, 30}
-};
 std::vector<std::string> paths {
     "test/GCF_000302455.1_ASM30245v1_genomic.fna.gz",
     "test/GCF_000762265.1_ASM76226v1_genomic.fna.gz",
@@ -33,7 +26,7 @@ std::vector<std::string> paths {
 
 TEST_CASE("hll") {
     std::vector<std::pair<uint64_t, uint64_t>> pairs {
-        {22, 36},
+        //{24, 40},
         {22, 26},
         {19, 24},
         {15, 21}
@@ -53,31 +46,6 @@ TEST_CASE("hll") {
     }
 }
 
-#if 0
-TEST_CASE("hll") {
-    //for(auto x: {8, 12, 16, 20, 24, 28, 32, 36, 42}) {
-    bool pass(1);
-    for(auto &pair: pairs) {
-        const size_t hsz(pair.first), val(1ull << pair.second);
-        hll::hll_t hll(hsz);
-#if CRAZY
-        std::unordered_set<decltype(pair.first)> t;
-        std::uint64_t r;
-        while(t.size() < val)
-            if(t.find(r = ((uint64_t)rand() << 32) | rand()) == t.end())
-                t.insert(r), hll.add(wang_hash(r));
-#else
-    for(size_t i(0); i != val; ++i) hll.add(wang_hash(((uint64_t)rand() << 32) | rand()));
-#endif
-        if(!hll.within_bounds(val))
-            fprintf(stderr, "Warning: Failing for %u, %u, with bound %lf not matching expected %lf. (val: %zu)\n",
-                    (unsigned)pair.first, (unsigned)pair.second,
-                    std::abs(hll.report() - val), hll.est_err(), val),
-            pass = 0;
-    }
-    REQUIRE(pass);
-}
-
 TEST_CASE("phll") {
     spvec_t vec;
     //static const size_t nps[] {23, 24, 25, 29, 30, 31};
@@ -92,10 +60,9 @@ TEST_CASE("phll") {
 }
 
 TEST_CASE("Invert Wang") {
-    for(size_t i(0); i < 10000uL; ++i) {
+    for(size_t i{0}; i < 10000uL; ++i) {
         REQUIRE(irving_inv_hash(wang_hash(i)) == i);
         uint64_t r(rand64());
         REQUIRE(irving_inv_hash(wang_hash(r)) == r);
     }
 }
-#endif
