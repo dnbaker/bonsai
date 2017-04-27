769716c9d643c0bf38bd05b6fc0057436e0c745f
dnbh <dnb@cs.jhu.edu>
2017-04-26 20:07:52 -0400

    one allocation for the sequence data.

--- lib/kseq_declare.h
+++ lib/kseq_declare.h
@@ -4,6 +4,7 @@
 #include "klib/kseq.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 
 KSEQ_INIT(gzFile, gzread)
 
@@ -33,6 +34,37 @@ static INLINE char *ksdup(const kstring_t *ks) {
 }
 
 // From bwa -- batch parsing.
+static inline void kseq2bseq1c(const kseq_t *ks, bseq1_t *s) // one chunk
+{ // TODONE: it would be better to allocate one chunk of memory.
+    const size_t size = ks->name.l + ks->comment.l + ks->seq.l + ks->qual.l + 4;
+    s->name = (char *)malloc(ks->name.l + ks->comment.l + ks->seq.l + ks->qual.l + 4);
+    memcpy(s->name, ks->name.s, ks->name.l + 1);
+    char *start = s->name + ks->name.l + 2;
+    if(ks->comment.l) {
+        s->comment = start;
+        memcpy(s->comment, ks->comment.s, ks->comment.l + 1);
+        start += ks->comment.l + 1;
+    } else s->comment = NULL;
+    s->seq = start;
+    memcpy(s->seq, ks->seq.s, ks->seq.l + 1);
+    start += ks->seq.l + 1;
+    if(ks->qual.l) {
+        s->qual = start;
+        memcpy(s->qual, ks->qual.s, ks->qual.l + 1);
+        start += ks->qual.l + 1;
+    } else s->qual = NULL;
+    s->l_seq   = ks->seq.l;
+    s->sam     = NULL;
+    s->l_sam   = 0;
+    s->id      = 0;
+}
+
+static inline void bseq_destroyc(bseq1_t *bs) {
+    free(bs->name);
+    free(bs->sam);
+}
+
+// From bwa -- batch parsing.
 static inline void kseq2bseq1(const kseq_t *ks, bseq1_t *s)
 { // TODO: it would be better to allocate one chunk of memory, but probably it does not matter in practice
     s->name    = ksdup(&ks->name);
@@ -46,6 +78,7 @@ static inline void kseq2bseq1(const kseq_t *ks, bseq1_t *s)
 }
 
 bseq1_t *bseq_read(int chunk_size, int *n_, void *ks1_, void *ks2_);
+bseq1_t *bseq_readc(int chunk_size, int *n_, void *ks1_, void *ks2_);
 
 static inline void bseq_destroy(bseq1_t *bs) {
     free(bs->name);
