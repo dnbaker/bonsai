b5bb2ab7257e2dbb8b679ffbc12b10d733aea848
dnbh <dnb@cs.jhu.edu>
2017-06-13 17:28:16 -0400

    Automatically generate utilities for additions of further class designations.
    Usage, add sample generated header/cpp files.

--- scripts/parse_nodes.py
+++ scripts/parse_nodes.py
@@ -13,18 +13,88 @@ def get_levels(path):
     return Counter(get_level(line) for line in open(path))
 
 
+def generate_enum(clades, maxl):
+    enum_str = "enum class ClassLvl:int {\n"
+    enum_str += "\n".join("    %s%s= %i," % (clade.upper(),
+                                             (maxl - len(clade) + 1) * ' ',
+                                             id)
+                          for id, clade in enumerate(clades))
+    enum_str += ("\n    ROOT %s= -1,"
+                 "\n    NO_RANK %s= -2\n};\n\n" % ((maxl - 4) * ' ',
+                                                   (maxl - 7) * ' '))
+    return enum_str
+
+
+def generate_class_names(clades):
+    reth = "extern const char *classlvl_arr[%i];\n" % (len(clades) + 2)
+    retc = "const char *classlvl_arr[%i] {\n" % (len(clades) + 2)
+    retc += "    \"no rank\",\n    \"root\"\n"
+    retc += "\n".join("    \"%s\"," % clade.lower() for clade in clades)
+    retc += "\n};\n\n"
+    return reth, retc
+
+
+def generate_class_map(clades, maxl):
+    reth = ("extern const std::unordered_map"
+            "<std::string, ClassLevel> classlvl_map;\n")
+    retc = "const std::unordered_map<std::string, ClassLevel> classlvl_map {\n"
+    retc += "    {\"no rank\", %sClassLevel::NO_RANK},\n" % ((maxl - 7) * ' ')
+    retc += "    {\"root\", %sClassLevel::ROOT},\n" % ((maxl - 4) * ' ')
+    retc += "\n".join("    {\"%s\", %sClassLevel::%s}," %
+                      (clade.lower(), (maxl - len(clade)) * ' ',
+                       clade.upper())
+                      for clade in clades)
+    retc += "\n};\n\n"
+    return reth, retc
+
+
+def generate_code(clades):
+    if not isinstance(clades,  list):
+        try:
+            clades = list(clades)
+        except:
+            raise RuntimeError("Clades is of type %s" % type(clades))
+    clades = [clade for clade in clades if clade not in
+              ("no", "rank", "no rank", "root")]
+    maxl = max(max(map(len, clades)), 7)
+    enumstr = generate_enum(clades, maxl)
+    hdr, retstr = generate_class_names(clades)
+    hdr = enumstr + hdr
+    chdr, cf = generate_class_map(clades, maxl)
+    retstr += cf
+    hdr += chdr
+    return hdr, retstr
+
+
+def print_levels():
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python " + sys.argv[0] + " <nodes.dmp>")
+        sys.exit(1)
+    print(repr(get_levels(sys.argv[1])))
+
+
+def main():
+    import sys
+    argv = sys.argv
+    for i in argv:
+        if i in ["-h", "--help", "-?"]:
+            sys.stderr.write("Usage: python %s <namesfile.txt> "
+                             "<out.h> <out.cpp>\n"
+                             "[Omit outfiles to write to "
+                             "stderr/stdout respectively.]\n" % argv[0])
+            sys.exit(1)
+    with open(argv[1]) if argv[1:] else stdout as f:
+        clades = [line.strip() for line in f]
+    doth, dotc = generate_code(clades)
+    print("//.h:\n" + doth,
+          file=open(argv[2], "w") if argv[2:] else sys.stderr)
+    print("//.cpp:\n" + dotc,
+          file=open(argv[3], "w") if len(argv) >= 3 else sys.stdout)
+    return 0
+
 
 '''
-  --- Changed:
-         'family': 8578,
-         'forma': 470,
-         'genus': 82587,
-         'no rank': 214584,
-         'order': 1418,
-         'subspecies': 20777,
-         'varietas': 6789
-
-
 Counter({'class': 309,
          'cohort': 3,
          'family': 8575,
@@ -55,41 +125,37 @@ Counter({'class': 309,
          'superphylum': 2,
          'tribe': 1996,
          'varietas': 6781})
-superkingdom
-kingdom
-subkingdom
-superphylum
-phylum
-subphylum
-superclass
-class
-subclass
-infraclass
-cohort
-superorder
-order
-suborder
-infraorder
-parvorder
-superfamily
-family
-subfamily
-tribe
-subtribe
-genus
-subgenus
-species group
-species subgroup
-species
-subspecies
-varietas
-forma
+Sorted:
+    superkingdom
+    kingdom
+    subkingdom
+    superphylum
+    phylum
+    subphylum
+    superclass
+    class
+    subclass
+    infraclass
+    cohort
+    superorder
+    order
+    suborder
+    infraorder
+    parvorder
+    superfamily
+    family
+    subfamily
+    tribe
+    subtribe
+    genus
+    subgenus
+    species group
+    species subgroup
+    species
+    subspecies
+    varietas
+    forma
 '''
 
-
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Usage: python " + sys.argv[0] + " <nodes.dmp>")
-        sys.exit(1)
-    print(repr(get_levels(sys.argv[1])))
+    sys.exit(main())
---
+++ scripts/sample_gen.h
@@ -0,0 +1,38 @@
+//.h:
+enum class ClassLvl:int {
+    SUPERKINGDOM = 0,
+    KINGDOM      = 1,
+    SUBKINGDOM   = 2,
+    SUPERPHYLUM  = 3,
+    PHYLUM       = 4,
+    SUBPHYLUM    = 5,
+    SUPERCLASS   = 6,
+    CLASS        = 7,
+    SUBCLASS     = 8,
+    INFRACLASS   = 9,
+    COHORT       = 10,
+    SUPERORDER   = 11,
+    ORDER        = 12,
+    SUBORDER     = 13,
+    INFRAORDER   = 14,
+    PARVORDER    = 15,
+    SUPERFAMILY  = 16,
+    FAMILY       = 17,
+    SUBFAMILY    = 18,
+    TRIBE        = 19,
+    SUBTRIBE     = 20,
+    GENUS        = 21,
+    SUBGENUS     = 22,
+    SPECIES      = 23,
+    SUBSPECIES   = 24,
+    GROUP        = 25,
+    SUBGROUP     = 26,
+    VARIETAS     = 27,
+    FORMA        = 28,
+    ROOT         = -1,
+    NO_RANK      = -2
+};
+
+extern const char *classlvl_arr[31];
+extern const std::unordered_map<std::string, ClassLevel> classlvl_map;
+
