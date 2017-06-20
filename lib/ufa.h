11a7a28407295412a1124e073aca6480507d5cb5
dnbh <dnb@cs.jhu.edu>
2017-06-20 17:33:33 -0400

    Add Classified, which converts any primitive into a class with identical properties/performance.

--- lib/ufa.h
+++ lib/ufa.h
@@ -9,20 +9,47 @@
 
 namespace emp { namespace ufa {
 
+// Converts any primitive into
+template<typename J>
+struct Classified {
+    J i;
+    operator       J&()       {return i;}
+    operator const J&() const {return i;}
+
+    Classified<J>() {}
+    template<typename T> Classified<J>(T j): i(j) {}
+};
+
+using Double   = Classified<double>;
+using Float    = Classified<float>;
+using Char     = Classified<char>;
+using Short    = Classified<short>;
+using Int      = Classified<int>;
+using Uint8_t  = Classified<uint8_t>;
+using Uint16_t = Classified<uint16_t>;
+using Uint32_t = Classified<uint32_t>;
+using Int8_t   = Classified<int8_t>;
+using Int16_t  = Classified<int16_t>;
+using Int32_t  = Classified<int32_t>;
+using Int64_t  = Classified<int64_t>;
+using Uint64_t = Classified<uint64_t>;
+using Size_t   = Classified<size_t>;
+using Voidptr  = Classified<void*>;
+
 // For composition
-template<typename T, typename size_type=std::uint8_t>
-struct uf_adapter: public T {
+template<typename Class, typename size_type=std::uint8_t>
+struct uf_adapter: public Class {
     size_type      r_;
     uf_adapter    *p_;
+
     template<typename... Args>
     uf_adapter(Args&&... args):
-        T(std::forward<Args...>(args)...), r_{0}, p_{this} {}
+        Class(std::forward<Args>(args)...), r_{0}, p_{this} {}
 } PACKED;
 
 template<typename T>
 T *find(T *node) {
-    if(node->p_ != node) node->p_ = find(node->p_);
-    return node->p_;
+    return node->p_ == node ? node: (node->p_ = find(node->p_));
 }
 
 template<typename T>
