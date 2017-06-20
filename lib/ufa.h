4562ea57aa5932c6f174a81a50653e9b93876eea
dnbh <dnb@cs.jhu.edu>
2017-06-20 17:43:57 -0400

    namespace std

--- lib/ufa.h
+++ lib/ufa.h
@@ -25,16 +25,16 @@ using Float    = Classified<float>;
 using Char     = Classified<char>;
 using Short    = Classified<short>;
 using Int      = Classified<int>;
-using Uint8_t  = Classified<uint8_t>;
-using Uint16_t = Classified<uint16_t>;
-using Uint32_t = Classified<uint32_t>;
-using Uint64_t = Classified<uint64_t>;
-using Int8_t   = Classified<int8_t>;
-using Int16_t  = Classified<int16_t>;
-using Int32_t  = Classified<int32_t>;
-using Int64_t  = Classified<int64_t>;
-using Uint64_t = Classified<uint64_t>;
-using Size_t   = Classified<size_t>;
+using Uint8_t  = Classified<std::uint8_t>;
+using Uint16_t = Classified<std::uint16_t>;
+using Uint32_t = Classified<std::uint32_t>;
+using Uint64_t = Classified<std::uint64_t>;
+using Int8_t   = Classified<std::int8_t>;
+using Int16_t  = Classified<std::int16_t>;
+using Int32_t  = Classified<std::int32_t>;
+using Int64_t  = Classified<std::int64_t>;
+using Uint64_t = Classified<std::uint64_t>;
+using Size_t   = Classified<std::size_t>;
 using Voidptr  = Classified<void*>;
 
 // For composition
