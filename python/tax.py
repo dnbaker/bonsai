0d03086a78fd9e115be3283c61d79a8139107da7
dnbaker <dnb@cs.jhu.edu>
2017-08-09 17:15:03 -0400

    Considerable progress in python taxonomy.

--- python/tax.py
+++ python/tax.py
@@ -3,10 +3,11 @@ from six import iteritems
 import sys
 import unittest
 from array import array as pa  # Python array
-from . import kmer
+import kmer
 import numpy as np
 import itertools
 from download_genomes import xfirstline
+from parse_nodes import generate_python_class_map
 
 
 def load_nameidmap(path):
@@ -16,12 +17,16 @@ def load_nameidmap(path):
 
 
 class TaxEntry(object):
-    def __init__(self, line, classlvl_map):
-        toks = [i.strip() for i in line.split('|')]
-        self.depth = classlvl_map[toks[2]]
+    def __init__(self, line, gi2taxmap, classlvl_map):
+        toks = (i.strip() for i in line.split('|'))
+        self.id = int(next(toks))
+        self.parent = int(next(toks))
+        try:
+            self.depth = classlvl_map[next(toks)]
+        except KeyError:
+            print("'" + "', '".join(str(i) for i in classlvl_map.keys()) + "'")
+            raise
         self.name = None
-        self.id = int(toks[0])
-        self.parent = int(toks[1])
         self.genome_paths = []
 
     def update(self, path):
@@ -52,15 +57,24 @@ class TaxEntry(object):
 class Taxonomy(object):
 
     def __init__(self, gi2taxpath, nodespath):
+        self.lvl_map = generate_python_class_map()
+        try:
+            assert "superkingdom" in self.lvl_map
+        except AssertionError:
+            print("Map string keys: " + ", ".join(i for i in self.lvl_map.keys() if isinstance(i, str)))
+            print("Map int keys: " + ", ".join(str(i) for i in self.lvl_map.keys() if isinstance(i, int)))
+            raise
         self.taxes = []
-        self.gi2taxmap = {line.split()[0]: int(line.split()[1]) for
-                          line in open(gi2taxpath)}
+        with open(gi2taxpath) as f:
+            self.gi2taxmap = {line.split()[0]: int(line.split()[1]) for
+                              line in f}
         self.nodes_path = nodespath
-        add_file(self.nodes_path)
+        self.add_file(self.nodes_path)
 
     def add_file(self, path):
-        for line in open(path):
-            add_line(line)
+        with open(path) as f:
+            for line in f:
+                self.add_line(line)
 
     def get_parents(self, el):
         parent = self.__getitem__(el)
@@ -75,32 +89,40 @@ class Taxonomy(object):
         raise NotImplementedError("Stuff")
 
     def __getitem__(self, el):
-        return self.taxes[el]
+        try:
+            return next(x for x in self.taxes if x == el or x.id == el)
+        except StopIteration:
+            raise KeyError("Missing element or id %s" % str(el))
 
     def __setitem__(self, key, val):
-        self.taxes[key] = val
+        try:
+            next(x for x in self.taxes if x == el or x.id == el).genome_paths.append(val)
+        except StopIteration:
+            raise KeyError("Missing element or id %s" % str(el))
 
     def __contains__(self, key):
-        return key in self.taxes
+        return key in self.taxes or any(tax.id == key for tax in self.taxes)
 
     def add_line(self, line):
-        taxes.append(TaxEntry(line, self.gi2taxmap))
+        self.taxes.append(TaxEntry(line, self.gi2taxmap, self.lvl_map))
 
     def add_genome_path(self, path, tax):
         try:
             assert isinstance(tax, int)
-            self.taxes[tax].genome_paths.append(path)
+            next(x for x in self.taxes if
+                 x.id == tax).genome_paths.append(path)
         except AssertionError:
             print("Assertion failed. Tax is not an int. Tax: %s, %s" %
                   (str(tax), repr(tax)))
             raise
-        except KeyError:
+        except StopIteration:
             print("Missing tax %i" % tax)
             raise
 
 
 if __name__ == "__main__":
     NAMEID_MAP_PATH = "save/combined_nip.txt"
+    NODES_PATH = "ref/nodes.dmp"
 
     class TC1(unittest.TestCase):
 
@@ -121,10 +143,7 @@ if __name__ == "__main__":
 
         def test_taxonomy_build(self):
             NODES_PATH = "ref/nodes.dmp"
-            tax = Taxonomy()
-            with open(NODES_PATH) as f:
-                for line in f:
-                    tax.add_line(line)
+            tax = Taxonomy(NAMEID_MAP_PATH, NODES_PATH)
             self.assertTrue(0 in tax)
 
     unittest.main()
