32287b2563ab36f12469a35655d9d676f5d37e62
dnbaker <dnb@cs.jhu.edu>
2017-09-08 23:16:50 -0400

    logic_error is a C++ exception, not a Python one.

--- python/tax.py
+++ python/tax.py
@@ -79,7 +79,7 @@ class TaxEntry(object):
                 return 0
             else return self.id < other.id
         else:
-            raise LogicError("Cannot compare TaxEntry to %s" % type(other))
+            raise NotImplementedError("Cannot currently compare TaxEntry to %s" % type(other))
 
     def __cmp__(self, other):
         if isinstance(self, type(other)):
