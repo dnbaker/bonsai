6f43a935f5e7b3ade523776c79b94c3ec876c5bd
dnbaker <dnb@cs.jhu.edu>
2018-01-06 04:15:49 -0500

    Handle multiple genomes sharing a tax id by adding 'nodes' above them.

--- lib/tx.h
+++ lib/tx.h
@@ -8,6 +8,7 @@
 #include "lib/feature_min.h"
 #include "lib/counter.h"
 #include "lib/bits.h"
+#include "lib/linearset.h"
 
 
 template<typename T>
@@ -29,29 +30,69 @@ using namespace std::literals;
 class TaxonomyReformation {
 protected:
     khash_t(p)           *pmap_;
-    khash_t(p)           *rpmap_;
     khash_t(name)    *name_map_;
     std::vector<tax_t> old_ids_;
-    std::unordered_map<std::string, tax_t> path_map;
-    uint64_t        counter_:63;
+    std::unordered_map<tax_t, std::vector<std::string>> path_map;
+    uint64_t        counter_:62;
     uint64_t          filled_:1;
+    uint64_t  panic_on_undef_:1;
 public:
     template<typename StrCon>
-    TaxonomyReformation(const char *name_path, const StrCon &paths):
-        pmap_{kh_init(p)}, rpmap_{kh_init(p)},
+    TaxonomyReformation(const char *name_path, const StrCon &paths,
+                        khash_t(p) *old_tax, bool panic_on_undef=false):
+        pmap_{kh_init(p)},
         name_map_{build_name_hash(name_path)},
         old_ids_{0, 1},
-        counter_(1), filled_(false)
+        counter_(1), filled_(false), panic_on_undef_(panic_on_undef)
     {
+        khash_t(p) *ct(kh_init(p));
+        kh_resize(p, ct, kh_size(old_tax));
+        std::memcpy(ct->keys, old_tax->keys, sizeof(tax_t) * old_tax->n_buckets);
+        std::memcpy(ct->vals, old_tax->vals, sizeof(tax_t) * old_tax->n_buckets);
+        std::memcpy(ct->flags, old_tax->flags, __ac_fsize(old_tax->n_buckets));
+        ct->n_buckets = old_tax->n_buckets;
+#define CP(x) ct->x = old_tax->x
+        CP(n_buckets); CP(size); CP(n_occupied); CP(upper_bound);
+#undef CP
         // Add the root of the tree.
         int khr;
-        khint_t ki{kh_put(p, pmap_, tax_t(counter_), &khr)};
+        khint_t ki(kh_put(p, pmap_, tax_t(counter_), &khr));
         kh_val(pmap_, ki) = 0;
-        ki = kh_put(p, rpmap_, 0, &khr);
-        kh_val(rpmap_, ki) = 1;
         fill_path_map(paths);
-        count::Counter<tax_t> ctr;
-        for(const auto &pair: path_map) ctr.add(pair.second);
+#if CHUNKY_BUT_FUNKY
+        std::unordered_set<tax_t> funkynodes; // Nodes with more than one genome which we'll have to break up.
+        for(const auto &pair: path_map)
+            if(pair.second.size() > 1)
+                funkynodes.insert(pair.first);
+#endif
+        std::mt19937 mt(std::hash<size_t>()(kh_size(old_tax) * path_map.size()));
+        std::unordered_map<tax_t, tax_t> old_to_new;
+        for(auto &pair: path_map) {
+            if(pair.second.size() > 1) {
+                for(auto &path: pair.second) {
+                    tax_t tmp(mt());
+                    while(kh_get(p, ct, tmp) != kh_end(ct)) tmp = mt();
+                    path_map[tmp] = {path};
+                    ki = kh_put(p, ct, tmp, &khr);
+                    kh_val(ct, ki) = pair.first;
+                }
+                std::vector<std::string> vec;
+                std::swap(vec, pair.second);
+            }
+        }
+        std::vector<tax_t> insertion_order;
+        insertion_order.reserve(path_map.size());
+        for(const auto &[tax, _]: path_map) insertion_order.push_back(tax);
+        pdqsort(std::begin(insertion_order), std::end(insertion_order),
+                [old_tax] (const tax_t a, const tax_t b) {
+            return node_depth(old_tax, a) < node_depth(old_tax, b);
+        });
+        for(const auto tax: insertion_order) {
+            old_to_new[tax] = old_ids_.size();
+            old_ids_.emplace_back(tax);
+            ki = kh_put(p, pmap_, static_cast<tax_t>(old_ids_.size()), &khr);
+            kh_val(pmap_, ki) = old_to_new.at(kh_val(ct, kh_get(p, ct, tax)));
+        }
     }
 
     template<typename T>
@@ -59,10 +100,13 @@ public:
         for(const std::string &path: container) {
             const tax_t id(get_taxid(get_str(path), name_map_));
             if(id == tax_t(-1)) {
-                LOG_WARNING("Tax id not found in path %s. Skipping. This can be fixed by augmenting the name dictionary file.\n", path);
+                if(panic_on_undef_)
+                    throw std::runtime_error(ks::sprintf("Tax id not found in path %s. Skipping. This can be fixed by augmenting the name dictionary file.\n", path).data());
+                else
+                    LOG_WARNING("Tax id not found in path %s. Skipping. This can be fixed by augmenting the name dictionary file.\n", path);
                 continue;
             }
-            path_map[path] = id;
+            path_map[id].push_back(path);
         }
     }
     
@@ -92,7 +136,6 @@ public:
     ~TaxonomyReformation() {
         khash_destroy(name_map_);
         khash_destroy(pmap_);
-        khash_destroy(rpmap_);
     }
 };
 using concise_tax_t = TaxonomyReformation;
