f18d5d26366277ceb2f87786bda83f38c6d7928e
dnbh <dnb@cs.jhu.edu>
2018-01-08 15:13:24 -0500

    Add DiskBitArray class for an on-disk bitmap, plus cleanup.

---
+++ lib/diskarray.cpp
@@ -0,0 +1,14 @@
+#include "diskarray.h"
+namespace ba {
+void allocate_file(std::FILE *fp, size_t nbytes) {
+    std::fseek(fp, 0, SEEK_SET); // Ensure we're at the beginning.
+    static const size_t bufsz(1 << 16);
+    std::vector<uint8_t> data(bufsz);
+    const int fn(fileno(fp));
+    while(nbytes & bufsz) {
+        ::write(fn, data.data(), bufsz);
+        nbytes -= bufsz;
+    }
+    ::write(fn, data.data(), nbytes);
+}
+}
