231a4cb7d92c96010dbdee457ea606da56da517c
dnbh <dnb@cs.jhu.edu>
2018-03-10 19:44:30 -0500

    Forgotten header file.

---
+++ bonsai/include/entropy.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "circular_buffer.h"
+#include "kmerutil.h"
+
+namespace emp {
+
+class CircusEnt {
+    circ::deque<char> q_;
+    uint8_t   counts_[4];
+    uint8_t        cqsz_;
+    const uint8_t   qsz_;
+    const double qszinv_;
+public:
+    static constexpr double NOT_FULL = -1.;
+    CircusEnt(unsigned qsz): q_(qsz), counts_{0}, cqsz_(0), qsz_(qsz), qszinv_(1./qsz) {
+        if(qsz == 0 || qsz > 255) throw std::runtime_error(std::string("Illegal queue size for ") + __PRETTY_FUNCTION__ + " with qsz = " + std::to_string(qsz));
+    }
+    CircusEnt(const CircusEnt &other): q_(other.q_), qsz_(other.qsz_), qszinv_(other.qszinv_) {
+        std::memcpy(counts_, other.counts_, sizeof(counts_));
+    }
+    void clear() {
+        *(reinterpret_cast<uint32_t *>(counts_)) = cqsz_ = 0;
+        q_.clear();
+    }
+    void push(char c) {
+#if !NDEBUG
+        if(cqsz_ == qsz_) {
+            auto to_pop = cstr_lut[q_.pop()];
+            assert(to_pop != static_cast<int8_t>(-1));
+            assert(counts_[to_pop]);
+            --counts_[to_pop];
+        }
+        q_.push(c);
+        assert(cstr_lut[c] != static_cast<int8_t>(-1));
+        ++counts_[cstr_lut[c]];
+        static_assert(std::is_unsigned_v<std::decay_t<decltype(q_.size())>>, "q's size should be unsigned.");
+        assert(unsigned(counts_[0] + counts_[1] + counts_[2] + counts_[3]) == q_.size());
+#else
+        // Increment count of added nucleotide
+        // If not fu
+        ++counts_[cstr_lut[c]];
+        if(cqsz_ == qsz_) {
+            --counts_[cstr_lut[q_.pop()]]; // Pop and decrement
+        } else {
+            ++cqsz_;                       // Or just keep filling the window
+        }
+        q_.push(c);
+#endif
+    }
+    double value() const {
+        if(unlikely(cqsz_ < qsz_)) return NOT_FULL;
+        assert(cqsz_ == q_.size());
+        double tmp(qszinv_ * (counts_[0])), sum(tmp * std::log2(tmp));
+        tmp = qszinv_ * counts_[1], sum += tmp * std::log2(tmp);
+        tmp = qszinv_ * counts_[2], sum += tmp * std::log2(tmp);
+        tmp = qszinv_ * counts_[3], sum += tmp * std::log2(tmp);
+        return sum;
+    }
+    double next_ent(char c) {
+        push(c);
+        return value();
+    }
+};
+
+} // namespace emp
