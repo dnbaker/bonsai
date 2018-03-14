adac5ef26c673c7d06444a6939f31f99cf203837
dnbh <dnb@cs.jhu.edu>
2018-03-13 21:30:05 -0400

    Extra reporting information for hll estimation.

--- bonsai/bin/distcmp.cpp
+++ bonsai/bin/distcmp.cpp
@@ -18,8 +18,8 @@ void usage(const char *arg) {
 
 //#define NOTHREADING
 
-void print_results(ks::string &ks, double sketchval, double exactval, const hll::hll_t &h1, const khash_t(set) *s1, const hll::hll_t &h2, const khash_t(set) *s2, size_t i, size_t j) {
-    ks.sprintf("%s\t%s\t%lf\t%lf\t%lf\t%lf\t%u\t%u\t%f\t%zu", argv[optind + i], argv[optind + j], sketchval, exactval, std::abs(sketchval - exactval), std::abs(sketchval - exactval) / exactval * 100., sketchsize, k, h1.report(), kh_size(s1), h2.report(), kh_size(s2));
+void print_results(ks::string &ks, unsigned sketchsize, double sketchval, double exactval, const hll::hll_t &h1, const khash_t(all) *s1, const hll::hll_t &h2, const khash_t(all) *s2, size_t i, size_t j, unsigned k, char *argv[], int optind) {
+    ks.sprintf("%s\t%s\t%lf\t%lf\t%lf\t%lf\t%u\t%u\t%lf\t%zu\n", argv[optind + i], argv[optind + j], sketchval, exactval, std::abs(sketchval - exactval), std::abs(sketchval - exactval) / exactval * 100., sketchsize, k, h1.creport(), kh_size(s1), h2.creport(), kh_size(s2));
 }
 
 class cmp_accumulonimbus {
@@ -34,12 +34,39 @@ class cmp_accumulonimbus {
     double jerrs_;
     double jerrsqs_;
     size_t nadded_;
+    size_t npadded_;
+public:
     cmp_accumulonimbus() {
         std::memset(this, 0, sizeof(*this));
     }
-    void add(const hll::hll_t &h1, const khash_t(set) *s1, const hll::hll_t &h2, const khash_t(set) *s2, double sketchval, double exactval) {
+    void add(const hll::hll_t &h, const khash_t(all) *s) {
         ++nadded_;
-        throw std::runtime_error("NotImplemenetd.");
+        double diff = kh_size(s) - h.creport();
+        herrs_ += diff;
+        iherrs_ += diff;
+        herrsqs_ += diff * diff;
+        iuerrs_ += diff * diff;
+    }
+    void add(const hll::hll_t &h1, const khash_t(all) *s1, const hll::hll_t &h2, const khash_t(all) *s2) {
+        ++npadded_;
+        auto us = union_size(s1, s2);
+        auto est_us = union_size(h1, h2);
+        auto diff = us - est_us;
+        uerrs_ += diff;
+        iuerrs_ += diff;
+        diff *= diff;
+        uerrsqs_ += diff;
+        iuerrsqs_ += diff;
+        auto ji = jaccard_index(s1, s2);
+        auto est_ji = jaccard_index(h1, h2);
+        diff = ji - est_ji;
+        jerrs_ += diff;
+        diff *= diff;
+        jerrsqs_ += diff;
+    }
+    auto report(std::FILE *fp=stdout) {
+        auto ret = std::fprintf(fp, "##total hll errors\tMSE hll\ttotal union errors\tMSE hll\ttotal ji errors\tSE ji\nNum sketches\tNum pairs\n");
+        ret     += std::fprintf(fp, "%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%zu\t%zu\n", herrs_, herrsqs_ / nadded_, uerrs_, uerrsqs_ / npadded_, jerrs_, jerrsqs_, nadded_, npadded_);
     }
 };
 
@@ -47,15 +74,16 @@ int main(int argc, char *argv[]) {
     int c;
     bool lowmem(false), canon(true), use_ertl(true);
     unsigned sketchsize(18), k(31);
-    FILE *ofp(stdout);
+    FILE *ofp(stdout), *sumfp(stdout);
     cmp_accumulonimbus accum;
-    while((c = getopt(argc, argv, "p:o:k:n:mECh")) >= 0) {
+    while((c = getopt(argc, argv, "s:p:o:k:n:mECh")) >= 0) {
         switch(c) {
             case 'E': use_ertl   = false; break;
             case 'C': canon      = false; break;
             case 'n': sketchsize = std::atoi(optarg); LOG_DEBUG("Set sketch size to %u with string '%s'\n", sketchsize, optarg); break;
             case 'k': k          = std::atoi(optarg); break;
             case 'o': ofp        = std::fopen(optarg, "w"); break;
+            case 's': sumfp      = std::fopen(optarg, "w"); break;
             case 'p': omp_set_num_threads(std::atoi(optarg)); break;
             case 'm': lowmem     = true; break;
             case 'h': case '?': usage(argv[0]); break;
@@ -85,6 +113,7 @@ int main(int argc, char *argv[]) {
             paths[0] = argv[optind + i];
             fill_set_genome<score::Lex>(argv[optind + i], sp, s1, i, nullptr, canon);
             hll_from_khash(h1, s1);
+            accum.add(h1, s1);
 #ifndef NOTHREADING
             #pragma omp parallel for
 #endif
@@ -93,9 +122,10 @@ int main(int argc, char *argv[]) {
                 hll_from_khash(h2, s2);
                 double sketchval = hll::jaccard_index(h1, h2);
                 double exactval  = emp::jaccard_index(s1, s2);
+                accum.add(h1, s1, h2, s2);
                 {
                     #pragma omp critical 
-                    print_results(ks, sketchval, exactval, h1, s1, h2, s2, i, j);
+                    print_results(ks, sketchsize, sketchval, exactval, h1, s1, h2, s2, i, j, k, argv, optind);
                 }
                 kh_clear(all, s2);
                 h2.clear();
@@ -121,19 +151,26 @@ int main(int argc, char *argv[]) {
             sketches.emplace_back(
                 make_hll(std::vector<std::string>{argv[optind + sketches.size()]},
                          k, k, sv, canon, nullptr, 1, sketchsize, nullptr /*kseq */, use_ertl));
+        
+        for(unsigned i = 0; i < ngenomes; ++i) {
+            accum.add(sketches[i], sets[i]);
+        }
         #pragma omp parallel for
-        for(size_t i(0); i < ngenomes; ++i) {
+        for(size_t i = 0; i < ngenomes; ++i) {
             for(size_t j(i + 1); j < ngenomes; ++j) {
                 sketchval = hll::jaccard_index(sketches[i], sketches[j]);
                 exactval  = emp::jaccard_index(sets[i], sets[j]);
                 {
                     #pragma omp critical
-                    print_results(ks, sketchval, exactval, sets[i], sketches[i], sets[j], sketches[j], i, j);
+                    accum.add(sketches[i], sets[i], sketches[j], sets[j]);
+                    print_results(ks, sketchsize, sketchval, exactval, sketches[i], sets[i], sketches[j], sets[j], i, j, k, argv, optind);
                     if(ks.size() >= 1 << 16) ks.write(fn), ks.clear();
                 }
             }
         }
     }
     ks.write(fn); ks.free();
+    accum.report(sumfp);
+    if(ofp != sumfp && sumfp != stdout) std::fclose(sumfp);
     if(ofp != stdout) std::fclose(ofp);
 }
--- bonsai/include/setcmp.h
+++ bonsai/include/setcmp.h
@@ -21,6 +21,15 @@ double jaccard_index(const KhashType *a, const KhashType *b) {
     const auto is(intersection_size(a, b));
     return static_cast<double>(is) / (kh_size(a) + kh_size(b) - is);
 }
+template<typename KhashType>
+size_t union_size(const KhashType *a, const KhashType *b) {
+    size_t ret(0);
+    for(khiter_t ki(0); ki != kh_end(a); ++ki)
+        if(kh_exist(a, ki))
+            ret += (khash_get(b, kh_key(a, ki)) == kh_end(b));
+    ret += kh_size(b);
+    return ret;
+}
 
 } // namespace emp
 
