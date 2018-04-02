82dc5831a47d5611ef58666d521b2e73f49dccc1
dnbaker <dnb@cs.jhu.edu>
2018-04-02 18:42:19 -0400

    Siphon off hll, setdist, sketch, and dist commands into flashdans executable.

--- bonsai/bin/bonsai.cpp
+++ bonsai/bin/bonsai.cpp
@@ -174,44 +174,6 @@ int phase2_main(int argc, char *argv[]) {
 }
 
 
-int hll_main(int argc, char *argv[]) {
-    int c, wsz(-1), k(31), num_threads(-1), sketch_size(24);
-    bool canon(true);
-    std::string spacing, paths_file;
-    std::ios_base::sync_with_stdio(false);
-    if(argc < 2) {
-        usage: LOG_EXIT("Usage: %s <opts> <paths>\nFlags:\n"
-                        "-k:\tkmer length (Default: 31. Max: 31)\n"
-                        "-w:\twindow size (Default: -1)  Must be -1 (ignored) or >= kmer length.\n"
-                        "-s:\tspacing (default: none). format: <value>x<times>,<value>x<times>,...\n"
-                        "   \tOmitting x<times> indicates 1 occurrence of spacing <value>\n"
-                        "-S:\tsketch size (default: 24). (Allocates 2 << [param] bytes of memory per HyperLogLog.\n"
-                        "-p:\tnumber of threads.\n"
-                        "-F:\tPath to file which contains one path per line\n"
-                        , argv[0]);
-    }
-    while((c = getopt(argc, argv, "Cw:s:S:p:k:tfh?")) >= 0) {
-        switch(c) {
-            case 'C': canon = false; break;
-            case 'h': case '?': goto usage;
-            case 'k': k = std::atoi(optarg); break;
-            case 'p': num_threads = std::atoi(optarg); break;
-            case 's': spacing = optarg; break;
-            case 'S': sketch_size = std::atoi(optarg); break;
-            case 'w': wsz = std::atoi(optarg); break;
-            case 'F': paths_file = optarg; break;
-        }
-    }
-    if(wsz < k) wsz = k;
-    std::vector<std::string> inpaths(paths_file.size() ? get_paths(paths_file.data())
-                                                       : std::vector<std::string>(argv + optind, argv + argc));
-    spvec_t sv(parse_spacing(spacing.data(), k));
-    LOG_INFO("Processing %zu paths with %i threads\n", inpaths.size(), num_threads);
-    const double est(estimate_cardinality<score::Lex>(inpaths, k, wsz, sv, canon, nullptr, num_threads, sketch_size));
-    std::fprintf(stderr, "Estimated number of unique exact matches: %lf\n", est);
-    return EXIT_SUCCESS;
-}
-
 int phase1_main(int argc, char *argv[]) {
     int c, taxmap_preparsed(0), use_hll(0), mode(score_scheme::LEX), wsz(-1), k(31), num_threads(-1), sketch_size(24);
     bool canon(true);
@@ -448,10 +410,6 @@ int main(int argc, char *argv[]) {
         {"build",    phase2_main},
         {"p2",       phase2_main},
         {"lca",      phase1_main},
-        {"hll",      hll_main},
-        {"dist",     bns::dist_main},
-        {"sketch",   bns::sketch_main},
-        {"setdist",  bns::setdist_main},
         {"hist",     hist_main},
         {"metatree", metatree_main},
         {"classify", classify_main}
--- bonsai/include/distmains.h
+++ bonsai/include/distmains.h
@@ -14,6 +14,7 @@ using MainFnPtr = int (*) (int, char **);
 int sketch_main(int, char **);
 int dist_main(int, char **);
 int setdist_main(int, char **);
+int hll_main(int, char **);
 }
 
 
