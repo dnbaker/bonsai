235493ce992f019cf3296222f8a3770149ea0989
dnbaker <dnb@cs.jhu.edu>
2018-04-02 18:42:19 -0400

    Siphon off hll, setdist, sketch, and dist commands into flashdans executable.

---
+++ bonsai/bin/flashdans.cpp
@@ -0,0 +1,17 @@
+#include "distmains.h"
+using namespace bns;
+
+void main_usage(char **argv) {
+    std::fprintf(stderr, "Usage: %s <subcommand> [options...]. Use %s <subcommand> for more options. [Subcommands: sketch, dist, setdist, hll.]\n",
+                 *argv, *argv);
+    std::exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 1) main_usage(argv);
+    if(std::strcmp(argv[1], "sketch") == 0) return sketch_main(argc - 1, argv + 1);
+    else if(std::strcmp(argv[1], "dist") == 0) return dist_main(argc - 1, argv + 1);
+    else if(std::strcmp(argv[1], "setdist") == 0) return setdist_main(argc - 1, argv + 1);
+    else if(std::strcmp(argv[1], "hll") == 0) return hll_main(argc - 1, argv + 1);
+    else throw std::runtime_error(std::string("Invalid subcommand ") + argv[1] + " provided.");
+}
