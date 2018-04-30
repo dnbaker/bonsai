c8eaa89f7a2b0db6cc447ff00f106142ff2e457e
dnbh <dnb@cs.jhu.edu>
2018-04-30 11:31:27 -0400

    Add ForPool (RAII wrapper around kt_forpool, which uses a threadpool to reduce process destruction/construction.

--- bonsai/include/util.h
+++ bonsai/include/util.h
@@ -111,6 +111,17 @@ void STLFREE(T &container) {
 } while(0)
 
 namespace bns {
+
+struct ForPool {
+    void *fp_;
+    ForPool(int nthreads): fp_(kt_forpool_init(nthreads)) {}
+    void forpool(void (*func)(void*,long,int), void *data, long n) {
+        kt_forpool(fp_, func, data, n);
+    }
+    ~ForPool() {
+        kt_forpool_destroy(fp_);
+    }
+};
 using MainFnPtr = int (*) (int, char **);
 
 using i32 = std::int32_t;
