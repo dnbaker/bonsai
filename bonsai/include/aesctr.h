b4a96124d5a4e0955c9232359744079f6cb468b6
dnbh <dnb@cs.jhu.edu>
2018-05-03 23:30:22 -0400

    Remove std::enable_if_t checks for is_integral because std::is_integral_v<__m*i> returns false. Add larger alignment to allow easy buffer_view usage.

--- bonsai/include/aesctr.h
+++ bonsai/include/aesctr.h
@@ -33,11 +33,23 @@ using std::size_t;
     seed_[index] = k;                                                    \
   } while (0)
 
+#ifndef HAS_AVX_512
+#  define HAS_AVX_512 (_FEATURE_AVX512F || _FEATURE_AVX512ER || _FEATURE_AVX512PF || _FEATURE_AVX512CD || __AVX512BW__ || __AVX512CD__ || __AVX512F__)
+#endif
+#if HAS_AVX_512
+#define VEC_ALIGNMENT_FOR_BUFFER 64
+#elif __AVX2__
+#define VEC_ALIGNMENT_FOR_BUFFER 32
+#else
+#define VEC_ALIGNMENT_FOR_BUFFER 16
+#endif
+
+
 
 template<typename GeneratedType=uint64_t, size_t UNROLL_COUNT=4, typename=std::enable_if_t<std::is_integral_v<GeneratedType>>>
 class AesCtr {
     static const size_t AESCTR_ROUNDS = 10;
-    uint8_t state_[sizeof(__m128i) * UNROLL_COUNT] __attribute__ ((aligned (sizeof(__m128i))));
+    uint8_t state_[sizeof(__m128i) * UNROLL_COUNT] __attribute__ ((aligned (VEC_ALIGNMENT_FOR_BUFFER)));
     __m128i ctr_[UNROLL_COUNT];
     __m128i seed_[AESCTR_ROUNDS + 1];
     __m128i work[UNROLL_COUNT];
@@ -137,7 +149,7 @@ public:
     const uint8_t *buf() const {return &state_[0];}
     using ThisType = AesCtr<GeneratedType, UNROLL_COUNT>;
 
-    template<typename T, typename=std::enable_if_t<std::is_integral_v<T>>>
+    template<typename T>
     class buffer_view {
         ThisType &ref;
     public:
@@ -157,7 +169,7 @@ public:
             return reinterpret_cast<pointer>(&ref.state_[BUFSIZE]);
         }
     };
-    template<typename T, typename=std::enable_if_t<std::is_integral_v<T>>>
+    template<typename T>
     buffer_view<T> view() {return buffer_view<T>(*this);}
 };
 #undef AES_ROUND
