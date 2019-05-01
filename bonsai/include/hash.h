437c06218217e14704bc8903b25c9934f377642c
dnbaker <dnb@cs.jhu.edu>
2019-05-01 08:54:33 -0400

    Clang compatibility.

--- bonsai/include/hash.h
+++ bonsai/include/hash.h
@@ -51,37 +51,29 @@ struct idt_struct {
 // https://naml.us/blog/tag/thomas-wang
 INLINE u64 irving_inv_hash(u64 key) {
   u64 tmp;
-
   // Invert key = key + (key << 31)
   tmp = key-(key<<31);
   key = key-(tmp<<31);
-
   // Invert key = key ^ (key >> 28)
   tmp = key^key>>28;
   key = key^tmp>>28;
-
   // Invert key *= 21
   key *= 14933078535860113213u;
-
   // Invert key = key ^ (key >> 14)
   tmp = key^key>>14;
   tmp = key^tmp>>14;
   tmp = key^tmp>>14;
   key = key^tmp>>14;
-
   // Invert key *= 265
   key *= 15244667743933553977u;
-
   // Invert key = key ^ (key >> 24)
   tmp = key^key>>24;
   key = key^tmp>>24;
-
   // Invert key = (~key) + (key << 21)
   tmp = ~key;
   tmp = ~(key-(tmp<<21));
   tmp = ~(key-(tmp<<21));
   key = ~(key-(tmp<<21));
-
   return key;
 }
 
