8efd8c4a1ab2085236086c07eca34e770eb59b50
Daniel Baker <daniel.n.baker@ibm.com>
2019-07-11 14:29:42 -0700

    shs2sketch, rolling multk progress.

--- python/load_shs.py
+++ python/load_shs.py
@@ -5,23 +5,26 @@ import numpy as np
 def load_from_path(path, minhash_sample=-1):
     def dec(x):
         return struct.unpack('l', x)[0]
-    
-    with gzip.open(path, "rb") as f:
-        s = f.read(8)
-        nelem = dec(s)
-        arr = np.zeros(nelem if minhash_sample < 0 else minhash_sample, dype=np.uint64)
-        s = f.read(8)
-        i = 0
-        if minhash_sample > 0:
-            while len(s) == 8 and minhash_sample < i:
-                arr[i] = dec(s)
-                i += 1
-                s = f.read(8)
-        else:
-            while len(s) == 8:
-                arr[i] = dec(s)
-                i += 1
-                s = f.read(8)
+
+    if int.from_bytes(open(path, "rb").read(2)) == 0x1f8b:
+        with gzip.open(path, "rb") as f:
+            s = f.read(8)
+            nelem = dec(s)
+            arr = np.zeros(nelem if minhash_sample < 0 else minhash_sample, dype=np.uint64)
+            s = f.read(8)
+            i = 0
+            if minhash_sample > 0:
+                while len(s) == 8 and minhash_sample < i:
+                    arr[i] = dec(s)
+                    i += 1
+                    s = f.read(8)
+            else:
+                while len(s) == 8:
+                    arr[i] = dec(s)
+                    i += 1
+                    s = f.read(8)
+    else:
+        arr = np.from_file(path, dtype=np.uint64)
     return arr
 
 
